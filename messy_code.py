c3b627813fc36d34d3713ac85e4ac1295270f03d
Javad Pourmostafa <javad.pourmostafa@gmail.com>
2025-06-30 14:19:16 +0200

    Update messy_code.py

--- messy_code.py
+++ messy_code.py
@@ -1,14 +1,9 @@
 import numpy as np
-import random
 import pandas as pd
+import random
 
-df=pd.DataFrame(np.random.randn(5,3),columns=['A','B','C'])
-
+data = np.random.rand(4, 3)
+df = pd.DataFrame(data, columns=["A", "B", "C"])
 print(df)
 
-x = np.random.randint(0,100)
-print("Value:", x)
-
-# no version logging
-# no seed
-# no structure
+print(np.random.randint(0, 100))
