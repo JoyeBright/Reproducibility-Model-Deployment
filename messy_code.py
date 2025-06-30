7d9f6d6678cc2a43c3fabd2abaaf0cffc1da7e88
Javad Pourmostafa <javad.pourmostafa@gmail.com>
2025-06-30 14:18:25 +0200

    Create messy_code.py

---
+++ messy_code.py
@@ -0,0 +1,14 @@
+import numpy as np
+import random
+import pandas as pd
+
+df=pd.DataFrame(np.random.randn(5,3),columns=['A','B','C'])
+
+print(df)
+
+x = np.random.randint(0,100)
+print("Value:", x)
+
+# no version logging
+# no seed
+# no structure
